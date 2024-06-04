025a6c3083d7f8858a9677a35f88474915f0bd2e
Paulo Reis <paulovitin@gmail.com>
2024-06-03 21:19:53 -0300

    feat: check expiration token, change entity names and add color, model e image url

--- haval-h6-mqtt/mqtt.js
+++ haval-h6-mqtt/mqtt.js
@@ -3,7 +3,12 @@ var slugify = require("slugify");
 
 require("dotenv").config();
 
-const { MQTT_HOST, MQTT_PASS, MQTT_USER } = process.env;
+const { MQTT_HOST, MQTT_PASS, MQTT_USER, VIN } = process.env;
+
+const device = {
+  "manufacturer": "GWM",
+  "model": "HAVAL H6"
+}
 
 const mqttModule = {
   connect() {
@@ -30,11 +35,11 @@ const mqttModule = {
         topic,
         JSON.stringify({
           device_class,
-          unique_id: `haval_${slugName}`,
+          unique_id: `haval_${VIN}_${slugName}`,
+          object_id: `haval_${VIN}_${slugName}`,
           name,
-          friendly_name: name,
           unit_of_measurement: unit,
-          state_topic: `haval/${code}/state`,
+          state_topic: `haval/${code}/state`
         }),
         (err) => {
           if (err) console.log(err);
